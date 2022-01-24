27575fe4c88d52a4a4299e6f8c272fbd9f337737
sere9ka <s.kovylov@zhiet.ru>
2022-01-24 17:11:54 +0300

    получаем данные

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import scroll from "./modules/scroll"
 import tabs from "./modules/tabs"
 import accordeon from "./modules/accordeon"
+import getData from "./modules/getData"
 
 seamless.polyfill();
 scroll()
 tabs()
 accordeon()
+getData()
